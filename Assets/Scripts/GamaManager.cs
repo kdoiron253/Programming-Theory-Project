4c7dc83de48a628d996e4192bfc9aa2e23ebed32
kdoiron253 <130408176+kdoiron253@users.noreply.github.com>
2023-08-05 19:50:25 -0700

    Pause menu
    Pause menu can be hidden on resume, or quits back to main menu.

---
+++ Assets/Scripts/GamaManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamaManager : MonoBehaviour
+{
+    public GameObject pauseScreen;
+
+
+    public void ResumeGame()
+    {
+        // hide the pause screen and set game back to active
+        pauseScreen.gameObject.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
