18ccb1a3ed78c2f9b62764fb771b2bc20f718f8a
kdoiron253 <130408176+kdoiron253@users.noreply.github.com>
2023-08-05 19:37:02 -0700

    Start Menu
    Start menu with start and exit buttons implemented

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class UIManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
